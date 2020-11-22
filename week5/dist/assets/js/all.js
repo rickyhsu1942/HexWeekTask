57b3f337a0a4c5547ca4079dc83048ec4de9f6f6
hao_hsu <hao_hsu@xinliwang.com.tw>
2020-11-23 00:39:35 +0800

    補上compile結果

---
+++ week5/dist/assets/js/all.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var url = window.location.pathname;
+$(".nav-link").each(function () {
+  $(this).removeClass("active");
+
+  if ($(this).attr("href") == url.split("/")[1]) {
+    $(this).addClass("active");
+  }
+});
+$(".dropdown").on("click", function () {
+  $(this).toggleClass("active");
+  $(".dropdown-items").toggleClass("active");
+});
+//# sourceMappingURL=all.js.map
