4f04100adec3e217a8562ec468fe45683f28bedd
RickyHsu <hao_hsu@xinliwang.com.tw>
2020-12-09 16:40:38 +0800

    add AOS

--- week6/app/assets/js/all.js
+++ week6/app/assets/js/all.js
@@ -2,3 +2,7 @@ $(".menu-icon").on("click", function (e) {
   e.preventDefault();
   $(".dropdown").toggleClass("active");
 });
+
+AOS.init({
+  once: true,
+  duration: 1000});
--- week6/dist/assets/js/all.js
+++ week6/dist/assets/js/all.js
@@ -4,4 +4,8 @@ $(".menu-icon").on("click", function (e) {
   e.preventDefault();
   $(".dropdown").toggleClass("active");
 });
+AOS.init({
+  once: true,
+  duration: 1000
+});
 //# sourceMappingURL=all.js.map
