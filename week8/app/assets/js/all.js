d58e929058a71c7211def6c39c799f24901afcb0
RickyHsu <hao_hsu@xinliwang.com.tw>
2020-12-17 17:22:39 +0800

    index: swiper

---
+++ week8/app/assets/js/all.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper('.swiper-container', {
+      spaceBetween: 30,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+});
