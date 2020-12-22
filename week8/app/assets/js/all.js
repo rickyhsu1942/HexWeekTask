b97d933c8a7806db9d6577bd417bc748ebf79e2c
RickyHsu <hao_hsu@xinliwang.com.tw>
2020-12-22 16:33:13 +0800

    nav

--- week8/app/assets/js/all.js
+++ week8/app/assets/js/all.js
@@ -1,9 +1,21 @@
-var swiper = new Swiper('.swiper-container', {
+var swiperBanner = new Swiper('.swiper-banner', {
+      spaceBetween: 30,
+      slidesPerView: 1,
+      loop: true,
+      navigation: {
+        nextEl: '.swiper-banner-button-next',
+        prevEl: '.swiper-banner-button-prev'
+      },
+});
+
+var swiperCards = new Swiper('.swiper-cards', {
+      width: 1110,
+      slidesPerView: 4,
+      slidesOffsetAfter: 300,
       spaceBetween: 30,
       loop: true,
       navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-cards-button-next'
       },
 });
 
