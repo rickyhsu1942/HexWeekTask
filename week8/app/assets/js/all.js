d4e5294a07f80502dd7c7c58b41de787b8b3a799
RickyHsu <hao_hsu@xinliwang.com.tw>
2020-12-25 15:01:28 +0800

    Popular Destinations & Get Inspiration

--- week8/app/assets/js/all.js
+++ week8/app/assets/js/all.js
@@ -9,7 +9,7 @@ var swiperBanner = new Swiper('.swiper-banner', {
 });
 
 var swiperCards = new Swiper('.swiper-cards', {
-      width: 1110,
+      width: 1050,
       slidesPerView: 4,
       slidesOffsetAfter: 300,
       spaceBetween: 30,
@@ -17,6 +17,30 @@ var swiperCards = new Swiper('.swiper-cards', {
       navigation: {
         nextEl: '.swiper-cards-button-next'
       },
+      breakpoints: {
+        768: {
+          width:1170
+        },
+        1200: {
+          width:1110
+        }
+      }
+});
+
+var swiperCards = new Swiper('.swiper-md-cards', {
+      width: 780,
+      slidesPerView: 3,
+      slidesOffsetAfter: 300,
+      spaceBetween: 30,
+      loop: true,
+      breakpoints: {
+        768: {
+          width:690
+        },
+        1200: {
+          width:1110
+        }
+      }
 });
 
 const textFields = [].map.call(document.querySelectorAll('.mdc-text-field'), function(el) {
